76f9aceb60bf1f5f08012a23d25a90cafc077434
GrandDexter <grand11.dexter@gmail.com>
2020-03-16 01:34:32 +0800

    add profile page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,7 +5,11 @@ const indexMiddleware = require('../middleware/index');
 const router = express.Router();
 
 router.get('/dashboard',indexMiddleware.isLoggedIn,(req,res) => {
-  res.render('dashboard/index',{user: req.user});
+  res.render('dashboard/index',{user: req.user,host: process.env.HOST});
+});
+
+router.get('/dashboard/profile',indexMiddleware.isLoggedIn,(req,res) => {
+  res.render('dashboard/profile',{user: req.user,host: process.env.HOST});
 });
 
 module.exports = router;
