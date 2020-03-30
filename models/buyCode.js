ef924d7a5cb02c2df0986623e5e8c371a2eb0159
GrandDexter <grand11.dexter@gmail.com>
2020-03-31 02:11:05 +0800

    test for payment api

--- models/buyCode.js
+++ models/buyCode.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
+// must delete after 24 hours if not verified
+// if payment succed delete the record and transfer to successfull payments
 const buyCode = new mongoose.Schema({
   email: String,
-  quantity: Number,
   phpPrice: Number,
   usdtPrice: Number,
+  disountedPercent:Number,
   totalPHP: Number,
   totalUSDT: Number,
   isValidated: {
     type: Boolean,
     default: false
-  }
+  },
+  dateValidated: Date
 });
 
 module.exports = mongoose.model('buyCode',buyCode);
--- routes/buyCode.js
+++ routes/buyCode.js
@@ -5,17 +5,19 @@ const helper = require('../lib/helpers');
 
 const router = express.Router();
 
-
+// request buycode page
 router.get('/buycode',(req,res) => {
-  // get the convertion
+  // get the php to usd convertion
   Convertion.findOne({},(err,convertionResult) => {
     if(!err && convertionResult){
+      // create buycode order data and discounts and format decimal
+      const disountPercent = 30;
       const data = {
-        quantity: 1,
-        phpPrice: 3500,
-        usdtPrice: 3500 / convertionResult.php,
-        totalPHP: 3500,
-        totalUSDT: 3500 / convertionResult.php,
+        phpPrice: helper.formatDecimalToString(3500),
+        usdtPrice: helper.formatDecimalToString(3500 / convertionResult.php),
+        disount: disountPercent,
+        totalPHP: helper.formatDecimalToString(3500-((disountPercent * 0.01) * 3500)),
+        totalUSDT: helper.formatDecimalToString((3500-((disountPercent * 0.01) * 3500)) / convertionResult.php),
         currentDate: new Date()
       }
       res.render('buyCode/buyReferralCode',{data: data,host: process.env.HOST});
@@ -26,6 +28,7 @@ router.get('/buycode',(req,res) => {
   });
 });
 
+// post and make transaction of buycode page
 router.post('/buycode',(req,res) => {
   let isValidAllField = true;
   // validate all fields 
@@ -46,18 +49,20 @@ router.post('/buycode',(req,res) => {
     Convertion.findOne({},(err,convertionResult) => {
       if(!err && convertionResult){
         // if no error and their is convertion data
+        const disountPercent = 30; 
         const data = {
           email: message.email.value,
-          quantity: 1,
-          phpPrice: 2450,
-          usdtPrice: 2450 / convertionResult.php,
-          totalPHP: 1 * 2450,
-          totalUSDT: (2450 / convertionResult.php) * 1,
+          phpPrice: 3500,
+          usdtPrice: 3500 / convertionResult.php,
+          disountedPercent: disountPercent,
+          totalPHP: 3500-((disountPercent * 0.01) * 3500),
+          totalUSDT: (3500-((disountPercent * 0.01) * 3500)) / convertionResult.php,
         }
+        // insert the buy code details
         BuyCode.create(data,(err,buyCodeData) => {
           if(!err && buyCodeData){
             // if no error redirect to payment information
-            const link = process.env.HOST + "/buycode/payment?ref=" + buyCodeData._id;
+            const link = process.env.HOST + "/buycode/payment?ref=" + buyCodeData._id + "&email=" + buyCodeData.email;
             helper.sendMail(message.email.value,"Buying Code for Registration",
               "Thank you for buying registration code. Please visit this link " + link + " to proceed to your payment.",(err) => {
                 if(!err){
@@ -84,11 +89,11 @@ router.post('/buycode',(req,res) => {
     Convertion.findOne({},(err,convertionResult) => {
       if(!err && convertionResult){
         const data = {
-          quantity: 1,
-          phpPrice: 3500,
-          usdtPrice: 3500 / convertionResult.php,
-          totalPHP: 3500,
-          totalUSDT: 3500 / convertionResult.php,
+          phpPrice: helper.formatDecimalToString(3500),
+          usdtPrice: helper.formatDecimalToString(3500 / convertionResult.php),
+          disount: disountPercent,
+          totalPHP: helper.formatDecimalToString(3500-((disountPercent * 0.01) * 3500)),
+          totalUSDT: helper.formatDecimalToString((3500-((disountPercent * 0.01) * 3500)) / convertionResult.php),
           currentDate: new Date()
         }
         res.render('buyCode/buyReferralCode',{data: data,host: process.env.HOST});
@@ -100,22 +105,60 @@ router.post('/buycode',(req,res) => {
   }
 });
 
+// get buycode payment page to verified the email
 router.get('/buycode/payment',(req,res) => {
-  if(typeof(req.query.ref) === 'string' && req.query.ref.trim().length > 0){
+  // check and validate the query string fields
+  const ref = typeof(req.query.ref) === 'string' && req.query.ref.trim().length > 0 ? req.query.ref.trim() : false;
+  const email = typeof(req.query.email) === 'string' && req.query.email.trim().length > 0 ? req.query.email.trim() : false
+
+  // if all fields are valid
+  if(ref && email){
+    // check if ref is valid
     BuyCode.findById(req.query.ref,(err,buyCodeResult) => {
       if(!err && buyCodeResult){
-        buyCodeResult.isValidated = true;
-        buyCodeResult.save((err) => {
-          if(!err){
-            res.render('buyCode/payment',{host: process.env.HOST});
-          } else {
-            res.redirect('/buycode');
+        // if no error and their is result
+        // check if email is ref value is refer to email
+        if(buyCodeResult.email === email){
+          // if email and ref value is valid
+          buyCodeResult.isValidated = true;
+          // update to validated and save the buycode details to database
+          // get the some data to pass to user
+          const data = {
+            totalUSD: Math.round((buyCodeResult.totalUSDT + Number.EPSILON) * 100) / 100,
+            transID: buyCodeResult._id
           }
-        });
+          buyCodeResult.save((err) => {
+            if(!err){
+              res.render('buyCode/payment',{host: process.env.HOST,data: data});
+            } else {
+              res.redirect('/buycode');
+            }
+          });
+        } else {
+          res.redirect('/buycode');
+        }
       } else {
         res.redirect('/buycode');
       }
     });
+  } else {
+    res.redirect('/buycode');
+  }
+});
+
+router.get("/buycode/payment/unconfirm",(req,res) => {
+  if(typeof(req.query.token) === 'string' && req.query.token.trim() === process.env.CALLBACK_TOKEN){
+    console.log(req.query);
+    res.render('buyCode/payment',{host: process.env.HOST});
+  } else {
+    res.redirect('/buycode');
+  }
+});
+
+router.get("/buycode/payment/success",(req,res) => {
+  if(typeof(req.query.token) === 'string' && req.query.token.trim() === process.env.CALLBACK_TOKEN){
+    console.log(req.query);
+    res.render('buyCode/payment',{host: process.env.HOST});
   } else {
     res.redirect('/buycode');
   }
